9c7117d0623aa7622d12ac71cb419ada5115fd1a
robsonrosa <robsonrosa@cwi.com.br>
2016-08-04 18:14:32 -0300

    Adding index file

---
+++ index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = require('./src/lib/hubot.js');
+require('./src/lib/assembler.js');
+require('./src/lib/bot-jenkins.js');
+require('./src/lib/log.js');
+require('./src/lib/message-handler.js');
+require('./src/lib/speech.js');
+require('./src/lib/speech.js');
